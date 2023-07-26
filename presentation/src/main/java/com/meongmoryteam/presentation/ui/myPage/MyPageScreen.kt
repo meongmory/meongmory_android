5e325330e63129ad4748803c994875c5ce44266d
arinming <jyandysys1@naver.com>
2023-07-27 02:44:05 +0900

    [feature/mypage] MyPage 액티비티 & 스크린 생성

---
+++ presentation/src/main/java/com/meongmoryteam/presentation/ui/myPage/MyPageScreen.kt
@@ -0,0 +1,13 @@
+package com.meongmoryteam.presentation.ui.myPage
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyPageScreen() {
+    Column {
+        Text(text = "마이페이지")
+        Text(text = "MyPage")
+    }
+}
