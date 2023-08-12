a21a5c7838842608d365f70ed8991eb39042d1af
sgsk88 <sgsk88@nate.com>
2023-08-13 04:46:09 +0900

    [feature/login_phone] 인증번호 화면 리팩터링 (#22)

--- presentation/src/main/java/com/meongmoryteam/presentation/ui/login/LoginScreen.kt
+++ presentation/src/main/java/com/meongmoryteam/presentation/ui/login/LoginScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,6 +21,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,66 +30,100 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.meongmoryteam.presentation.R
+import com.meongmoryteam.presentation.base.TextButtonComponent
+import com.meongmoryteam.presentation.base.TextFieldComponent
+import com.meongmoryteam.presentation.ui.theme.ButtonContent
 import com.meongmoryteam.presentation.ui.theme.DarkGrey
+import com.meongmoryteam.presentation.ui.theme.LightGrey
+import com.meongmoryteam.presentation.ui.theme.NotoSansKR
+import com.meongmoryteam.presentation.ui.theme.Orange
 import com.meongmoryteam.presentation.ui.theme.Typography
 
 @Composable
-fun LoginScreen(
-    loginViewModel: LoginViewModel = hiltViewModel(),
-) {
-    val loginViewState by loginViewModel.viewState.collectAsState()
-
+fun LoginScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
-        Icon(
-            modifier = Modifier.padding(12.dp),
-            painter = painterResource(R.drawable.ic_left_btn),
-            contentDescription = stringResource(R.string.login_back_button),
-        )
-        Spacer(modifier = Modifier.padding(30.dp))
-        Text(
-            modifier = Modifier.padding(start = 16.dp),
-            text = stringResource(R.string.login_phone_screen_title),
-            fontSize = 20.sp,
-            color = Color.Black,
-            fontWeight = FontWeight.SemiBold
-        )
-        Spacer(modifier = Modifier.padding(6.dp))
-        Text(
-            modifier = Modifier.padding(start = 16.dp),
-            text = stringResource(R.string.login_phone_screen_sub_title),
-            color = DarkGrey,
-            style = Typography.titleSmall
-        )
-        Spacer(modifier = Modifier.padding(32.dp))
-        Text(
-            modifier = Modifier.padding(start = 16.dp),
-            text = stringResource(R.string.login_phone),
-            color = DarkGrey,
-            style = Typography.titleSmall
-        )
-        Spacer(modifier = Modifier.padding(10.dp))
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(37.dp)
-                .padding(horizontal = 16.dp),
-            value = loginViewState.phoneNumber,
-            onValueChange = { loginViewModel.setEvent(LoginContract.LoginEvent.OnPhoneChanged(it)) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
-        )
-        Spacer(modifier = Modifier.padding(6.dp))
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            onClick = { loginViewModel.setEvent(LoginContract.LoginEvent.GetCertificationButtonClicked) },
-        ) {
-            
-        }
+        LoginPhoneCertTitle()
+        LoginPhoneTextField()
+    }
+}
+
+@Composable
+fun LoginPhoneCertTitle() {
+    Icon(
+        modifier = Modifier.padding(12.dp),
+        painter = painterResource(R.drawable.ic_left_btn),
+        contentDescription = stringResource(R.string.login_back_button),
+    )
+    Spacer(modifier = Modifier.padding(30.dp))
+
+    Text(
+        modifier = Modifier.padding(start = 16.dp),
+        text = stringResource(R.string.login_phone_screen_title),
+        fontSize = 20.sp,
+        color = Color.Black,
+        fontWeight = FontWeight.SemiBold
+    )
+    Spacer(modifier = Modifier.padding(6.dp))
+
+    Text(
+        modifier = Modifier.padding(start = 16.dp),
+        text = stringResource(R.string.login_phone_screen_sub_title),
+        color = DarkGrey,
+        style = Typography.titleSmall
+    )
+    Spacer(modifier = Modifier.padding(32.dp))
+}
+
+@Composable
+fun LoginPhoneTextField(
+    loginViewModel: LoginViewModel = hiltViewModel(),
+) {
+    val loginViewState by loginViewModel.viewState.collectAsState()
+
+    Text(
+        modifier = Modifier.padding(start = 16.dp),
+        text = stringResource(R.string.login_phone),
+        color = DarkGrey,
+        style = Typography.titleSmall
+    )
+    Spacer(modifier = Modifier.padding(10.dp))
+
+    TextFieldComponent(
+        name = loginViewState.phoneNumber,
+        placeholder = stringResource(R.string.login_phone_text_field_hint),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(37.dp)
+            .padding(horizontal = 16.dp),
+        onValueChange = { loginViewModel.setEvent(LoginContract.LoginEvent.OnPhoneChanged(it.trim())) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    Spacer(modifier = Modifier.padding(6.dp))
+
+    TextButtonComponent(
+        text = stringResource(R.string.login_phone_get_certification_button),
+        colors = if (loginViewState.phoneNumber.length != 11) {
+            ButtonDefaults.textButtonColors(LightGrey)
+        } else {
+            ButtonDefaults.textButtonColors(Orange)
+        },
+        style = TextStyle(
+            fontFamily = NotoSansKR,
+            fontWeight = FontWeight.W500,
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            color = ButtonContent,
+            platformStyle = PlatformTextStyle(includeFontPadding = false)
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+    ) {
+        loginViewModel.setEvent(LoginContract.LoginEvent.GetCertificationButtonClicked)
     }
 }
 
